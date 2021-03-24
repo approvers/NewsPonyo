7653209c33308599952eb11fca2879a1ad399e48
soshiharami <soshi0807sss@gmail.com>
2021-03-24 22:58:24 +0900

    fix: delete some

--- src/main/scala/newsPonyo/DB/Query.scala
+++ src/main/scala/newsPonyo/DB/Query.scala
@@ -2,12 +2,8 @@ package newsPonyo.DB
 
 import newsPonyo.SendNews
 import org.javacord.api.entity.channel.TextChannel
-import org.javacord.api.entity.message.embed.EmbedBuilder
-import org.javacord.api.event.message.MessageCreateEvent
-import org.mongodb.scala.model.Filters.equal
 import org.mongodb.scala.{Document, MongoClient, MongoCollection}
 
-import java.awt.Color
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
@@ -15,9 +11,6 @@ import scala.util.{Failure, Success}
 
 object Query {
     def newsQuety(client: MongoClient, coll: MongoCollection[Document], count: Int, channel: TextChannel): Unit = {
-        println(math
-            .random() * 1000
-            .toInt)
         val query = coll.find()
             .skip((math
                 .random() * 1000)
