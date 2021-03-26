b64d630ea65c70e30c59ec02033b31d2d51ecfd9
soshiharami <soshi0807sss@gmail.com>
2021-03-26 22:28:45 +0900

    fix: format

--- src/main/scala/newsPonyo/AddNews.scala
+++ src/main/scala/newsPonyo/AddNews.scala
@@ -7,62 +7,56 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 
 object AddNews extends Command {
-    override val commandName: String = "add"
+  override val commandName: String = "add"
 
-    def variation(string: Array[String]): Either[String, Unit] = {
-        string
-            .length match {
-            case 1 => Left("引数不足")
-            case 2 | 3 => _
-            case _ => Left("引数過度")
-        }
+  def variation(string: Array[String]): Either[String, Unit] = {
+    string.length match {
+      case 1     => Left("引数不足")
+      case 2 | 3 => _
+      case _     => Left("引数過度")
+    }
+  }
+
+  def addNews(
+      event: MessageCreateEvent,
+      args: Array[String]
+    ): Either[String, Unit] = {
+    val title = args.apply(1)
+
+    val name = args.length match {
+      case 3 => args.apply(2)
+      case _ => event.getMessage.getAuthor.getDisplayName
     }
 
-    def addNews(event: MessageCreateEvent, args: Array[String]): Either[String, Unit] = {
-        val title = args.apply(1)
+    val client = DataBase.connectDB()
+    val database = client.getDatabase("News")
 
-        val name = args
-            .length match {
-            case 3 => args.apply(2)
-            case _ => event
-                .getMessage
-                .getAuthor
-                .getDisplayName
-        }
+    val coll = database.getCollection("News")
 
-        val client = DataBase.connectDB()
-        val database = client.getDatabase("News")
+    Right(
+        Insert
+          .addNews(coll, name, title)
+          .onComplete {
+            case Success(result) =>
+              client.close()
+              event.getChannel
+                .sendMessage(s"Success add new news \nThis news id is ${result.getInsertedId
+                  .asObjectId()
+                  .getValue
+                  .toString}")
+            case Failure(exception) =>
+              Left(exception.toString)
+          }
+    )
+  }
 
-        val coll = database.getCollection("News")
+  override def command(event: MessageCreateEvent): Either[String, Unit] = {
+    val args = event.getMessage.getContent
+      .split(" ")
+    variation(args)
+    Right(addNews(event, args))
+  }
 
-
-        Right(
-            Insert
-                .addNews(coll, name, title)
-                .onComplete {
-                    case Success(result) =>
-                        client.close()
-                        event.getChannel
-                            .sendMessage(s"Success add new news \nThis news id is ${
-                                result
-                                    .getInsertedId
-                                    .asObjectId()
-                                    .getValue
-                                    .toString
-                            }")
-                    case Failure(exception) =>
-                        Left(exception
-                            .toString)
-                })
-    }
-
-    override def command(event: MessageCreateEvent): Either[String, Unit] = {
-        val args = event.getMessage
-            .getContent
-            .split(" ")
-        variation(args)
-        Right(addNews(event, args))
-    }
-
-    override val help: String = "p!add [title] [name]? :: 記事を書けます。\n使用例 [p!add hoge huga]"
+  override val help: String =
+    "p!add [title] [name]? :: 記事を書けます。\n使用例 [p!add hoge huga]"
 }
