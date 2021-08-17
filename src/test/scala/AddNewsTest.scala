13130f85985055cf7a33825dea3c1b18ddbb2fb8
soshiharami <soshi0807sss@gmail.com>
2021-08-18 00:42:26 +0900

    fix: test code variation function

--- src/test/scala/AddNewsTest.scala
+++ src/test/scala/AddNewsTest.scala
@@ -1,13 +1,22 @@
-import newsPonyo.AddNews
+import newsPonyo._
 import org.scalatest.FunSuite
 
 class AddNewsTest extends FunSuite {
   test("ValidateTest error") {
-    assert(AddNews.variation(Array.fill(1)("TEST")) === Left("引数不足"))
-    assert(AddNews.variation(Array.fill(10)("TEST")) === Left("引数過度"))
+    assert(
+        AddNews.variation(null, Array.fill(1)("TEST")) === Left(
+            Faild(null, "引数が少ないです。タイトルも入力してください")
+        )
+    )
+    assert(
+        AddNews
+          .variation(null, Array.fill(10)("TEST")) === Left(
+            Faild(null, "引数が多いです。余計なスペースなどがないか確認してください\n※タイトルには半角スペースが使えません")
+        )
+    )
   }
 
   test("ValidateTest Success") {
-    assert(AddNews.variation(Array.fill(2)("TEST")) === Right({}))
+    assert(AddNews.variation(null, Array.fill(2)("TEST")) === Right({}))
   }
 }
